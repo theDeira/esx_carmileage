1d99d458ec767f9e3a75ae8ef52ac119a9b46df8
Yiğit <40323418+theDeira@users.noreply.github.com>
2020-01-05 15:26:00 +0300

    main.lua - 5664 komutu

--- server/main.lua
+++ server/main.lua
@@ -66,3 +66,46 @@ ESX.RegisterServerCallback('esx_carmileage:getMileage', function(source, cb, pla
 	)
 
 end)
+
+function round(num, numDecimalPlaces)
+  local mult = 10^(numDecimalPlaces or 0)
+  return math.floor(num * mult + 0.5) / mult
+end
+
+RegisterCommand('5664', function(source, args)  --araç transfer komutu
+
+	
+	myself = source
+	local xPlayer = ESX.GetPlayerFromId(myself)
+	local plate1 = args[1]
+  
+	if plate1 ~= nil then plate01 = plate1 else plate01 = "" end
+  
+  
+
+
+	MySQL.Async.fetchScalar(
+        'SELECT km FROM veh_km WHERE carplate = @plate',
+        {
+            ['@plate'] = plate1
+        },
+        function(result)
+            if result then
+				TriggerClientEvent('esx:showNotification', myself, "~h~~c~Biraz bekleyin,~w~ belirttiğiniz ~b~plaka ~g~sorgulanıyor...")  
+				Citizen.Wait(5000)
+				TriggerClientEvent('esx:showNotification', myself, "~w~PLAKA: ~c~"..plate1.."~w~ - ~g~"..(round(result / 1000) * 1.0 ).."KM") 
+				Citizen.Wait(5000)
+				TriggerClientEvent('esx:showNotification', myself, "~g~$100~w~ sorgu ücreti alınmıştır!") 
+				xPlayer.removeMoney(100)
+				--TriggerClientEvent('esx:showNotification', myself, "1")  
+			else
+				--TriggerClientEvent('esx:showNotification', myself, "2")  
+				TriggerClientEvent('esx:showNotification', myself, "~h~~c~Biraz bekleyin,~w~ belirttiğiniz ~b~plaka ~g~sorgulanıyor...")  
+				Citizen.Wait(5000)
+				TriggerClientEvent('esx:showNotification', myself, '~b~Karakol ~w~veritabanında böyle bir ~y~plaka ~r~yok!')  
+            end
+		
+        end
+    )
+	
+end)
